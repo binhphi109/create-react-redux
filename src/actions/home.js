82fc5ce01bf3fab6ba3653cf2d8765eabef0fa97
Binh-Phi Nguyen <kugoo109@gmail.com>
2018-04-05 23:40:56 +0200

    add sample async loadContent

--- src/actions/home.js
+++ src/actions/home.js
@@ -1,7 +1,11 @@
 import * as actionTypes from '../constants/actionTypes';
 
+const serviceBase = 'http://localhost:3001/api/v1/';
+
 export const loadContent = () => (dispatch) => {
-  return setTimeout(() => {
-    dispatch({ type: actionTypes.LOAD_CONTENT, content: 'Hello World' });
-  }, 3000);
+  return fetch(serviceBase + 'content')
+    .then(response => response.json())
+    .then(data => {
+      dispatch({ type: actionTypes.LOAD_CONTENT, content: data });
+    });
 };
