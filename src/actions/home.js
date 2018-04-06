197d8ecb4b4775706e4a37119d6a3dcfb01c322d
Binh-Phi Nguyen <kugoo109@gmail.com>
2018-04-06 09:46:57 +0200

    change name of apiUrl

--- src/actions/home.js
+++ src/actions/home.js
@@ -1,9 +1,9 @@
 import * as actionTypes from '../constants/actionTypes';
 
-const serviceBase = 'http://localhost:3001/api/v1/';
+const apiUrl = 'http://localhost:3001/api/v1/';
 
 export const loadContent = () => (dispatch) => {
-  return fetch(serviceBase + 'content')
+  return fetch(apiUrl + 'content')
     .then(response => response.json())
     .then(data => {
       dispatch({ type: actionTypes.LOAD_CONTENT, content: data });
