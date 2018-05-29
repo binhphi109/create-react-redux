4804b0401dc2ca17133da908bedec8e140b8363c
Binh-Phi Nguyen <binh.phi.nguyen@rwth-aachen.de>
2018-05-29 22:21:33 +0200

    fix devToolsExtension error

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import reducers from './reducers/index';
 
 export const history = createHistory();
 
-export const store = createStore(reducers, compose(
-  applyMiddleware(thunk, routerMiddleware(history)),
-  window.devToolsExtension && window.devToolsExtension()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(thunk, routerMiddleware(history))
 ));
